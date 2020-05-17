3b24f5bd0db3d1663034d8e0e797887f942622fe
Andraz Tori <atori@outbrain.com>
2020-05-17 14:14:30 +0200

    New cache format, faster, smaller
    Former-commit-id: 32ec52872129c3a957a2a3c301d91cbbdcc7a7ea

--- fwumious_wabbit/src/parser.rs
+++ fwumious_wabbit/src/parser.rs
@@ -12,6 +12,9 @@ const RECBUF_LEN:usize = 2048;
 pub const HEADER_LEN:usize = 2;
 pub const NAMESPACE_DESC_LEN:usize = 1;
 pub const LABEL_OFFSET:usize = 1;
+pub const IS_NOT_SINGLE_MASK : u32 = 1u32 << 31;
+pub const MASK31: u32 = !IS_NOT_SINGLE_MASK;
+pub const NULL: u32= IS_NOT_SINGLE_MASK; // null is just an exact IS_NOT_SINGLE_MASK
 
 pub struct VowpalParser<'a> {
     input_bufread: &'a mut dyn BufRead,
@@ -24,8 +27,11 @@ pub struct VowpalParser<'a> {
 
 /* 
 organization of records buffer 
-(offset u16, len u16)[number_of_features]
-(u32)[dynamic_number_of_hashes] 
+(u32) length of the output record
+(union_u u32)[number of features], where:
+    -- if the most significant bit is zero, this is a single value feature and bits 1-31 are a hash
+    -- if the most significant bit is zero, then, 15 next bits are the start offset, and 16 next bits are the end offset of features beyond the buffer 
+(u32)[dynamic_number_of_hashes]
 */
 
 impl<'a> VowpalParser<'a> {
@@ -61,13 +67,12 @@ impl<'a> VowpalParser<'a> {
                 Ok(n) => n,
                 Err(e) => Err(e)?          
             };
-            // Fields are: 
-            // u32 length of the output record
-            // u32 label
-            // (u32) * number of namespaces - (where u32 is really two u16 indexes - begining and end of the features belonging to a namespace) 
-            let mut bufpos: usize = (self.vw_map.num_namespaces as usize) + HEADER_LEN;
-            self.output_buffer.truncate(bufpos);
-            for i in &mut self.output_buffer[0..bufpos] { *i = 0 }
+            {
+                let mut bufpos: usize = (self.vw_map.num_namespaces as usize) + HEADER_LEN;
+                self.output_buffer.truncate(bufpos);
+                for i in &mut self.output_buffer[0..bufpos] { *i = NULL };
+            }
+            let mut current_char_num_of_features = 0;
 
             unsafe {
                 let p = self.tmp_read_buf.as_ptr();
@@ -98,27 +103,41 @@ impl<'a> VowpalParser<'a> {
                     if *p.add(i_start) == 0x7c { // "|"
                         // As we get new namespace index, we store end of the last one
                         //println!("Closing index {} {}", current_char_index, bufpos);
-                        *buf.add(current_char_index) = ((bufpos_namespace_start<<16) + bufpos) as u32;
                         current_char = *p.add(i_start+1) as usize;
                         current_char_index = self.vw_map.lookup_char_to_index[current_char] * NAMESPACE_DESC_LEN + HEADER_LEN;
-                        bufpos_namespace_start = bufpos;
+                        current_char_num_of_features = 0;
                     } else { 
                         // We have a feature! Let's hash it and write it to the buffer
                         // println!("item out {:?}", std::str::from_utf8(&rr.tmp_read_buf[i_start..i_end]));
-                        let h = murmur3::hash32_with_seed(&self.tmp_read_buf[i_start..i_end], *self.namespace_hash_seeds.get_unchecked(current_char));  
-                        self.output_buffer.push(h);
-                        bufpos += 1;
+                        let h = murmur3::hash32_with_seed(&self.tmp_read_buf[i_start..i_end], 
+                                                          *self.namespace_hash_seeds.get_unchecked(current_char)) & MASK31;  
+                        //self.output_buffer.push(h);
+                        if current_char_num_of_features == 0 {
+                            *buf.add(current_char_index) = h;
+                        } else if current_char_num_of_features == 1 {
+                            // We need to promote feature currently written in-place to out of place
+                            bufpos_namespace_start = self.output_buffer.len();
+                            self.output_buffer.push(*buf.add(current_char_index));
+                            // Then add also out of place character
+                            self.output_buffer.push(h);
+                            // Now we store current pointers to in-place location, with IS_NOT_SINGLE_MASK marker
+                            let bufpos = self.output_buffer.len();
+                            *buf.add(current_char_index) = IS_NOT_SINGLE_MASK | (((bufpos_namespace_start<<16) + bufpos) as u32);
+                        } else {
+                            // Now push a new value and store the new pointer to in_place
+                            self.output_buffer.push(h);
+                            let bufpos = self.output_buffer.len();
+                            *buf.add(current_char_index) = IS_NOT_SINGLE_MASK | (((bufpos_namespace_start<<16) + bufpos) as u32);
+                        }
+                        current_char_num_of_features += 1;
                     }
                     i_end += 1;
                     i_start = i_end ;
                 }
-                *buf.add(current_char_index) = ((bufpos_namespace_start<<16) + bufpos) as u32;
-
-                *buf = bufpos as u32;
-                self.output_buffer.set_len(bufpos);  // Why do we also keep length as first datapoint - so we can directly write these records to cache
             }
             
 //            println!("item out {:?} {}", self.output_buffer, bufpos);
+            self.output_buffer[0] = self.output_buffer.len() as u32;
             Ok(&self.output_buffer)
         }
 
@@ -163,10 +182,10 @@ r#"1 |A a
 "#);
         let mut rr = VowpalParser::new(&mut buf, &vw);
         // we test a single record
-        assert_eq!(rr.next_vowpal().unwrap(), [6, 1, nd(5,6),           0, 0, 2988156968]);
-        assert_eq!(rr.next_vowpal().unwrap(), [6, 0, nd(0,5), nd(5,6), 0, 2422381320]);
-        assert_eq!(rr.next_vowpal().unwrap(), [7, 1, nd(5,7),           0, 0, 2988156968, 3529656005]);
-        assert_eq!(rr.next_vowpal().unwrap(), [7, 0, nd(5,6), nd(6, 7), 0, 2988156968, 2422381320]);
+        assert_eq!(rr.next_vowpal().unwrap(), [5, 1, 2988156968 & MASK31, NULL, NULL]);
+        assert_eq!(rr.next_vowpal().unwrap(), [5, 0, NULL, 2422381320 & MASK31, NULL]);
+        assert_eq!(rr.next_vowpal().unwrap(), [7, 1, nd(5,7) | IS_NOT_SINGLE_MASK, NULL, NULL, 2988156968 & MASK31, 3529656005 & MASK31]);
+        assert_eq!(rr.next_vowpal().unwrap(), [5, 0, 2988156968 & MASK31, 2422381320 & MASK31, NULL]);
         //println!("{:?}", rr.output_buffer);
         // now we test if end-of-stream works correctly
         assert_eq!(rr.next_vowpal().unwrap().len(), 0);
