888c1a9a8e18aedf511b84232833b77a7e3c9522
Andraz Tori <atori@outbrain.com>
2020-06-18 23:13:17 +0200

    add structure to weights - it's less flexible, but code is more readable
    Former-commit-id: 2823a59a481fedb3021dce83d54082bf3c794470

--- fwumious_wabbit/profile.sh
+++ fwumious_wabbit/profile.sh
@@ -1,2 +1,2 @@
-perf record --call-graph dwarf,16384 -e cpu-clock -F 997 -- $1 && perf script \
+perf record --call-graph dwarf,16384 -e cache-misses,cpu-clock -F 997 -- $1 && perf script \
 | FlameGraph/stackcollapse-perf.pl | FlameGraph/rust-unmangle | FlameGraph/flamegraph.pl > flame.svg && firefox flame.svg
