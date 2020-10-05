77f663f5ebefb447d8ec05d024cc6f73acb709c6
Yonatan Karni <ykarni@outbrain.com>
2020-10-05 17:30:32 +0300

    added predict measurement + reporting standard deviation

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -1,7 +1,6 @@
 import math
 import os.path
 import sys
-from collections import defaultdict
 
 import memit
 import psutil
@@ -25,8 +24,11 @@ def cleanup():
     rm_quietly("hard.vw")
 
 
-def clean_caches():
+def vw_clean_cache():
     rm_quietly("train.vw.gz.cache")
+
+
+def fw_clean_cache():
     rm_quietly("train.vw.gz.fwcache")
 
 
@@ -58,26 +60,41 @@ def time_bash_cmd(cmd):
     return memit.memit(cmd)
 
 
-def benchmark_one_train_cycle(vw_train_cmd, fw_train_cmd):
-    clean_caches()
-    res = dict()
-    if benchmark_vw:
-        res["vw"] = train_model(vw_train_cmd)
-        # print(f"vw train time: {vw_train_time} seconds, using {vw_train_mem} KB and {vw_train_cpu} CPU")
-        # print(f"vw train time - with cache: {vw_train_time_with_cache} seconds, using {vw_mem_with_cache} KB and {vw_cpu_with_cache} CPU")
-    if benchmark_fw:
-        res["fw"] = train_model(fw_train_cmd)
-        # print(f"fw train time: {fw_train_time} seconds, using {fw_train_mem} KB and {fw_train_cpu} CPU")
-        # print(f"fw train time - with cache: {fw_train_time_with_cache} seconds, using {fw_mem_with_cache} KB and {fw_cpu_with_cache} CPU")
-
-    return res
-
-
 def train_model(cmd):
     vw_results = dict()
     vw_results["no_cache"] = time_bash_cmd(cmd)
     vw_results["with_cache"] = time_bash_cmd(cmd)
     return vw_results
+
+
+def benchmark_cmd(cmd, times, run_before=None, ):
+    benchmark_means = [0., 0., 0.]
+    results = []
+
+    for _ in range(times):
+        if run_before:
+            run_before()
+
+        result = time_bash_cmd(cmd)
+        results.append(result)
+
+        for i in range(len(benchmark_means)):
+            benchmark_means[i] += result[i] / float(times)
+
+    benchmark_stds = [0., 0., 0.]
+
+    for result in results:
+        for i in range(len(benchmark_stds)):
+            benchmark_stds[i] += math.pow(result[i] - benchmark_means[i], 2) / float(times)
+
+    for i in range(len(benchmark_stds)):
+        benchmark_stds[i] = math.sqrt(benchmark_stds[i])
+
+    return benchmark_means, benchmark_stds
+
+
+def format_metrics(times, means, stds):
+    return f"{means[0]:.2f} ± {stds[0]:.2f} seconds, {means[1]/1024:.0f} ± {stds[1]/1024:.0f} MB, {means[2]*100:.2f} ± {stds[2]:.0f}% CPU ({times} runs)"
 
 
 if __name__ == "__main__":
@@ -95,6 +112,9 @@ if __name__ == "__main__":
     vw_train_cmd = "vw --data train.vw.gz -l 0.1 -b 25 -c --adaptive --sgd --loss_function logistic --link logistic --power_t 0.0 --l2 0.0 --hash all --final_regressor vw_model --save_resume --interactions AB"
     fw_train_cmd = "../target/release/fw --data train.vw.gz -l 0.1 -b 25 -c --adaptive --fastmath --sgd --loss_function logistic --link logistic --power_t 0.0 --l2 0.0 --hash all --final_regressor fw_model --save_resume --interactions AB"
 
+    vw_predict_cmd = "vw --data easy.vw -t -b 25 -p vw_preds.out --link logistic --initial_regressor vw_model --hash all --interactions AB"
+    fw_predict_cmd = "../target/release/fw --data easy.vw --sgd --adaptive -t -b 25 -p fw_preds.out --initial_regressor fw_model --link logistic --hash all --interactions AB"
+
     if action == "cleanup" or action == "generate" or action == "all":
         cleanup()
 
@@ -103,36 +123,24 @@ if __name__ == "__main__":
         generate.generate()
         gzip_file("train.vw")
 
+    times = 2
+
     if action == "train" or action == "all":
-        benchmark_means = defaultdict(dict)
-        repetitions = 10
-        train_results = []
-        for _ in range(repetitions):
-            train_result = benchmark_one_train_cycle(vw_train_cmd, fw_train_cmd)
-            train_results.append(train_result)
-            for model in train_result.keys():
-                for setting in train_result[model].keys():
-                    if setting not in benchmark_means[model].keys():
-                        benchmark_means[model][setting] = [0., 0., 0.]
+        vw_train_no_cache_benchmark_means, vw_train_no_cache_benchmark_stds = benchmark_cmd(vw_train_cmd, times, vw_clean_cache)
+        fw_train_no_cache_benchmark_means, fw_train_no_cache_benchmark_stds = benchmark_cmd(fw_train_cmd, times, fw_clean_cache)
 
-                    for i in range(len(benchmark_means[model][setting])):
-                        benchmark_means[model][setting][i] += train_result[model][setting][i] / float(repetitions)
+        print(f"vw train, no cache: {format_metrics(times, vw_train_no_cache_benchmark_means, vw_train_no_cache_benchmark_stds)}")
+        print(f"fw train, no cache: {format_metrics(times, fw_train_no_cache_benchmark_means, fw_train_no_cache_benchmark_stds)}")
 
-        benchmark_stds = defaultdict(dict)
-        for train_result in train_results:
-            for model in train_result.keys():
-                for setting in train_result[model].keys():
-                    if setting not in benchmark_stds[model].keys():
-                        benchmark_stds[model][setting] = [0., 0., 0.]
+        vw_train_with_cache_benchmark_means, vw_train_with_cache_benchmark_stds = benchmark_cmd(vw_train_cmd, times)
+        fw_train_with_cache_benchmark_means, fw_train_with_cache_benchmark_stds = benchmark_cmd(fw_train_cmd, times)
 
-                    for i in range(len(benchmark_stds[model][setting])):
-                        benchmark_stds[model][setting][i] += math.pow(train_result[model][setting][i] - benchmark_means[model][setting][i], 2) / float(repetitions)
+        print(f"vw train, using cache: {format_metrics(times, vw_train_with_cache_benchmark_means, vw_train_with_cache_benchmark_stds)}")
+        print(f"fw train, using cache: {format_metrics(times, fw_train_with_cache_benchmark_means, fw_train_with_cache_benchmark_stds)}")
 
-        for model in benchmark_stds.keys():
-            for setting in benchmark_stds[model].keys():
-                for i in range(len(benchmark_stds[model][setting])):
-                    benchmark_stds[model][setting][i] = math.sqrt(benchmark_stds[model][setting][i])
+    if action == "predict" or action == "all":
+        vw_predict_no_cache_benchmark_means, vw_predict_no_cache_benchmark_stds = benchmark_cmd(vw_predict_cmd, times)
+        fw_predict_no_cache_benchmark_means, fw_predict_no_cache_benchmark_stds = benchmark_cmd(fw_predict_cmd, times)
 
-        print(benchmark_means)
-        print(benchmark_stds)
-
+        print(f"vw predict, no cache: {format_metrics(times, vw_predict_no_cache_benchmark_means, vw_predict_no_cache_benchmark_stds)}")
+        print(f"fw predict, no cache: {format_metrics(times, fw_predict_no_cache_benchmark_means, fw_predict_no_cache_benchmark_stds)}")
