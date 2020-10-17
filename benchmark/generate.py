d3b31ca8330f1367af500b62917b67c3be8c63f3
Andraz Tori <atori@outbrain.com>
2020-10-17 22:26:24 +0200

    Furthe benchmarking improvements

--- benchmark/generate.py
+++ benchmark/generate.py
@@ -27,7 +27,7 @@ def get_score(a, b):
 NUM_RANDOM_FEATURES = 10
 
 def random_features():
-    l = [" "]
+    l = [""]
     for x in range(NUM_RANDOM_FEATURES):
         namespace = chr(ord('C') + x)
         l.append("|" + namespace + " " + namespace + str(random.randint(0, 10000)))
