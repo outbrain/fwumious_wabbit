ac2c4a055fa47d5e47e8a403e47648808d7883c2
Andraz Tori <atori@outbrain.com>
2020-12-13 22:04:10 +0100

    Huh

---
+++ src/multithread_helpers.rs
@@ -0,0 +1,88 @@
+use std::sync::Arc;
+use std::sync::Mutex;
+use std::mem::ManuallyDrop;
+use core::ops::{Deref, DerefMut};
+use std::mem;
+use std::marker::PhantomData;
+use crate::regressor::RegressorTrait;
+
+
+
+// This is a helper for UNSAFELY sharing data between threads
+
+pub struct UnsafelySharableTrait<T:Sized> {
+    content: ManuallyDrop<T>,
+    reference_count: Arc<Mutex<PhantomData<u32>>>,
+}
+
+pub type BoxedRegressorTrait  = UnsafelySharableTrait<Box<dyn RegressorTrait>>;
+
+// SUPER UNSAFE
+// SUPER UNSAFE
+// SUPER UNSAFE
+// This literary means we are on our own -- but it is the only way to implement HogWild performantly
+unsafe impl <T:Sized>Sync for UnsafelySharableTrait<T> {}
+unsafe impl <T:Sized>Send for UnsafelySharableTrait<T> {}
+
+impl <T:Sized>Deref for UnsafelySharableTrait<T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        &self.content
+    }
+}
+
+impl <T:Sized>DerefMut for UnsafelySharableTrait<T> {
+    fn deref_mut(&mut self) -> &mut T {
+        &mut self.content
+    }
+}
+
+
+impl <T:Sized>Drop for UnsafelySharableTrait<T> {
+    fn drop(&mut self) {
+        unsafe {
+            // we are called before reference is removed, so we need to decide if to drop it or not
+            let count = Arc::<Mutex<PhantomData<u32>>>::strong_count(&self.reference_count) - 1;
+            if count == 0 {
+                let box_to_be_dropped = ManuallyDrop::take(&mut self.content);
+                // Now this means that the content will be dropped
+                println!("Dropping BoxedRegressorTrait!");
+            } else {
+                println!("Not dropping BoxedRegressorTrait as there are still {} references!", count);
+            }
+        }
+        
+    }
+}
+
+
+impl <T:Sized + 'static> UnsafelySharableTrait<T> {
+    pub fn new(a: T) -> UnsafelySharableTrait<T>
+    {
+        UnsafelySharableTrait::<T> {
+            content: ManuallyDrop::new(a),
+            reference_count: Arc::new(Mutex::new(std::marker::PhantomData{})),
+        }
+    }
+}
+
+// Non-generalized implementation
+// Todo - generalize this[A
+
+impl BoxedRegressorTrait {
+    pub fn clone(&self) -> BoxedRegressorTrait {
+        // UNSAFE AS HELL
+        unsafe {
+            // Double deref here sounds weird, but you got to know that dyn Trait and Box<dyn Trait> are the same thing, just box owns it.
+            // And you can get dyn Trait content, but you can't get box content (directly)
+            let r2: Box<dyn RegressorTrait> = mem::transmute(& *self.content.deref().deref());
+            let ret = BoxedRegressorTrait{
+                content: ManuallyDrop::new(r2),
+                reference_count: self.reference_count.clone()        
+            };
+            println!("References to object: {}", Arc::<Mutex<PhantomData<u32>>>::strong_count(&ret.reference_count));
+            ret
+        }
+    }
+}
