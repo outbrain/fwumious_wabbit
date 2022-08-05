31b32fd8fedab285c9db2f8c0c64b9460226377e
Andraz Tori <atori@outbrain.com>
2022-08-05 17:04:58 +0200

    default to true

--- src/block_neural.rs
+++ src/block_neural.rs
@@ -30,7 +30,7 @@ use block_helpers::{Weight, WeightAndOptimizerData, OptimizerData};
 use blas::*;
 
 const MAX_NUM_INPUTS:usize= 16000;
-const USE_BLAS:bool = false;
+const USE_BLAS:bool = true;
 
 #[derive(PartialEq, Debug)]
 pub enum NeuronType {
