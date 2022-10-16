b0dbbbdd84dcf90fe06f82aad9f39f2fc9347f3b
bskrlj <skblaz@github.com>
2022-10-16 23:43:23 +0200

    Added log-loss awk-based metric

--- examples/ffm/run_fw_with_prediction_tests.sh
+++ examples/ffm/run_fw_with_prediction_tests.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -e
 
+#####################################################################################
+# A script tailored for fast inspection(s) of fw's output space and its properties. #
+# Complains if:																	    #
+# 1. Predictions with different weight files don't match						    #
+# 2. Predictions look too random on simple data sets							    #
+# 3. Not high-enough balanced accuracy margin is observed w.r.t. random			    #
+#####################################################################################
+
 function compute_main_metrics {
 	# A method that takes err1/2 counts from context and overwrites main statistics
 
@@ -10,6 +18,26 @@ function compute_main_metrics {
 	SENSITIVITY=$(bc <<<"scale=5 ; $TP / $ALL_INSTANCES_POSITIVE")
 	SPECIFICITY=$(bc <<<"scale=5 ; $TN / $ALL_INSTANCES_NEGATIVE")
 	BALANCED_ACCURACY=$(bc <<<"scale=5 ; ($SENSITIVITY + $SPECIFICITY) / 2")
+	LOGLOSS=$(cat $LOGLOSS_FRAME | awk 'BEGIN {
+			 totalLoss=0;
+			 allInstances=0;
+			 eps=10e-8;
+		 }
+		 {
+			 # Encode negative class appropriately
+			 if($2==-1)
+				 $2=0.0;
+
+			 # L_{\log}(y, p) = -(y log (p) + (1 - y) log (1 - p))
+			 termFirst = $2 * log($1 + eps);
+			 termSecond = (1.0 - $2) * log(1.0 - $1 + eps);
+			 localLoss = -(termFirst + termSecond);
+			 totalLoss += localLoss;
+			 allInstances += 1;
+		 }
+		 END {print totalLoss / allInstances}');
+
+	
 }
 
 SCRIPT=$(readlink -f "$0")
@@ -70,7 +98,7 @@ $FW $namespaces $rest -i models/inference_weights.fw.model -d $DATASET_FOLDER/tr
 ###########################################
 
 # Create ground truth labels first
-cat datasets/train.vw | mawk '{print $1}' >predictions/ground_truth.txt
+cat datasets/train.vw | awk '{print $1}' >predictions/ground_truth.txt
 
 # get last n predictions of training
 cat ./predictions/training.txt | tail -n $(cat predictions/ground_truth.txt | wc -l) >./predictions/training_eval_part_only.txt
@@ -100,7 +128,7 @@ mv ./tmp.txt ./predictions/joint_prediction_space.txt
 ALL_INSTANCES=$(cat predictions/joint_prediction_space.txt | wc -l)
 
 # Are inference weights' predictions the same?
-INFERENCE_SAME_COUNT=$(cat ./predictions/joint_prediction_space.txt | mawk '$2==$3' | wc -l)
+INFERENCE_SAME_COUNT=$(cat ./predictions/joint_prediction_space.txt | awk '$2==$3' | wc -l)
 
 if [ $ALL_INSTANCES = $INFERENCE_SAME_COUNT ]; then
 	echo "$INFO_STRING All inferences' weights' predictions are the same .."
@@ -132,19 +160,26 @@ if [ $NUM_UNIQUE_TRAINING_RUN_EVAL = 1 ]; then
 
 fi
 
+#######################################
+# PART 1 - benchmarks of training set #
+#######################################
+
 # Create a benchmark against random classifier
-echo -e "OUTPUT_TAG\tTHRESHOLD\tPRECISION\tRECALL\tF1\tBALANCED_ACCURACY"
+echo -e "OUTPUT_TAG\tTHRESHOLD\tPRECISION\tRECALL\tF1\tBALANCED_ACCURACY\tLOGLOSS"
 ALL_INSTANCES=$(cat predictions/joint_prediction_space.txt | wc -l)
-ALL_INSTANCES_POSITIVE=$(cat predictions/joint_prediction_space.txt | mawk '{print $4}' | grep -v '\-1' | wc -l)
-ALL_INSTANCES_NEGATIVE=$(cat predictions/joint_prediction_space.txt | mawk '{print $4}' | grep '\-1' | wc -l)
+ALL_INSTANCES_POSITIVE=$(cat predictions/joint_prediction_space.txt | awk '{print $4}' | grep -v '\-1' | wc -l)
+ALL_INSTANCES_NEGATIVE=$(cat predictions/joint_prediction_space.txt | awk '{print $4}' | grep '\-1' | wc -l)
 
-TP=$(cat predictions/joint_prediction_space.txt | mawk -v THRESHOLD="$THRESHOLD" '($4=="1") &&  ($3>=THRESHOLD) {positiveMatch++} END {print positiveMatch}')
+TP=$(cat predictions/joint_prediction_space.txt | awk -v THRESHOLD="$THRESHOLD" '($4=="1") &&  ($3>=THRESHOLD) {positiveMatch++} END {print positiveMatch}')
 
-TN=$(cat predictions/joint_prediction_space.txt | mawk -v THRESHOLD="$THRESHOLD" '($4=="-1") &&  ($3<THRESHOLD) {positiveMatch++} END {print positiveMatch}')
+TN=$(cat predictions/joint_prediction_space.txt | awk -v THRESHOLD="$THRESHOLD" '($4=="-1") &&  ($3<THRESHOLD) {positiveMatch++} END {print positiveMatch}')
 
-FP=$(cat predictions/joint_prediction_space.txt | mawk -v THRESHOLD="$THRESHOLD" '($4=="-1") &&  ($3>=THRESHOLD) {positiveMatch++} END {print positiveMatch}')
+FP=$(cat predictions/joint_prediction_space.txt | awk -v THRESHOLD="$THRESHOLD" '($4=="-1") &&  ($3>=THRESHOLD) {positiveMatch++} END {print positiveMatch}')
 
-FN=$(cat predictions/joint_prediction_space.txt | mawk -v THRESHOLD="$THRESHOLD" '($4=="1") &&  ($3<THRESHOLD) {positiveMatch++} END {print positiveMatch}')
+FN=$(cat predictions/joint_prediction_space.txt | awk -v THRESHOLD="$THRESHOLD" '($4=="1") &&  ($3<THRESHOLD) {positiveMatch++} END {print positiveMatch}')
+
+cat predictions/joint_prediction_space.txt | awk '{print $3" "$4}' > ./predictions/logloss_frame_training.txt;
+LOGLOSS_FRAME="./predictions/logloss_frame_training.txt";
 
 # Account for corner cases
 if [ "$FP" = "" ]; then
@@ -156,20 +191,23 @@ if [ "$FN" = "" ]; then
 fi
 
 compute_main_metrics
-echo -e "FW\t$THRESHOLD\t$PRECISION\t$RECALL\t$F1\t$BALANCED_ACCURACY"
+echo -e "FW\t$THRESHOLD\t$PRECISION\t$RECALL\t$F1\t$BALANCED_ACCURACY\t$LOGLOSS"
 
 # Random baseline
-TP=$(cat predictions/joint_prediction_space.txt | mawk -v THRESHOLD="$THRESHOLD" '($4=="1") &&  (rand()>=THRESHOLD) {positiveMatch++} END {print positiveMatch}')
+TP=$(cat predictions/joint_prediction_space.txt | awk -v THRESHOLD="$THRESHOLD" '($4=="1") &&  (rand()>=THRESHOLD) {positiveMatch++} END {print positiveMatch}')
 
-TN=$(cat predictions/joint_prediction_space.txt | mawk -v THRESHOLD="$THRESHOLD" '($4=="-1") &&  (rand()<THRESHOLD) {positiveMatch++} END {print positiveMatch}')
+TN=$(cat predictions/joint_prediction_space.txt | awk -v THRESHOLD="$THRESHOLD" '($4=="-1") &&  (rand()<THRESHOLD) {positiveMatch++} END {print positiveMatch}')
 
-FP=$(cat predictions/joint_prediction_space.txt | mawk -v THRESHOLD="$THRESHOLD" '($4=="-1") &&  (rand()>=THRESHOLD) {positiveMatch++} END {print positiveMatch}')
+FP=$(cat predictions/joint_prediction_space.txt | awk -v THRESHOLD="$THRESHOLD" '($4=="-1") &&  (rand()>=THRESHOLD) {positiveMatch++} END {print positiveMatch}')
 
-FN=$(cat predictions/joint_prediction_space.txt | mawk -v THRESHOLD="$THRESHOLD" '($4=="1") &&  (rand()<THRESHOLD) {positiveMatch++} END {print positiveMatch}')
+FN=$(cat predictions/joint_prediction_space.txt | awk -v THRESHOLD="$THRESHOLD" '($4=="1") &&  (rand()<THRESHOLD) {positiveMatch++} END {print positiveMatch}')
+
+cat predictions/joint_prediction_space.txt | awk '{print rand()" "$4}' > ./predictions/logloss_frame_random.txt;
+LOGLOSS_FRAME="./predictions/logloss_frame_random.txt";
 
 compute_main_metrics
 
-echo -e "RANDOM\t$THRESHOLD\t$PRECISION\t$RECALL\t$F1\t$BALANCED_ACCURACY"
+echo -e "RANDOM\t$THRESHOLD\t$PRECISION\t$RECALL\t$F1\t$BALANCED_ACCURACY\t$LOGLOSS"
 
 # Is the difference substantial (in BA)
 BA_DIFF=$(bc <<<"scale=5 ; $BALANCED_ACCURACY_FW - $BALANCED_ACCURACY")
@@ -187,23 +225,25 @@ fi
 # Test inference weights on a given data set
 $FW $namespaces $rest -i models/inference_weights.fw.model -d $DATASET_FOLDER/test-hard.vw -t -p ./predictions/test_hard_predictions.txt
 
-cat ./datasets/test-hard.vw | mawk '{print $1}' >./predictions/hard_ground_truth.txt
+cat ./datasets/test-hard.vw | awk '{print $1}' >./predictions/hard_ground_truth.txt
 paste predictions/test_hard_predictions.txt predictions/hard_ground_truth.txt >./predictions/joint_hard_predictions_and_ground.txt
 
-ALL_INSTANCES_POSITIVE=$(cat predictions/joint_hard_predictions_and_ground.txt | mawk '{print $2}' | grep -v '\-1' | wc -l)
-ALL_INSTANCES_NEGATIVE=$(cat predictions/joint_hard_predictions_and_ground.txt | mawk '{print $2}' | grep '\-1' | wc -l)
+ALL_INSTANCES_POSITIVE=$(cat predictions/joint_hard_predictions_and_ground.txt | awk '{print $2}' | grep -v '\-1' | wc -l)
+ALL_INSTANCES_NEGATIVE=$(cat predictions/joint_hard_predictions_and_ground.txt | awk '{print $2}' | grep '\-1' | wc -l)
 
 # Random baseline
-TP=$(cat predictions/joint_hard_predictions_and_ground.txt | mawk -v THRESHOLD="$THRESHOLD" '($2=="1") &&  ($1>=THRESHOLD) {positiveMatch++} END {print positiveMatch}')
+TP=$(cat predictions/joint_hard_predictions_and_ground.txt | awk -v THRESHOLD="$THRESHOLD" '($2=="1") &&  ($1>=THRESHOLD) {positiveMatch++} END {print positiveMatch}')
 
-TN=$(cat predictions/joint_hard_predictions_and_ground.txt | mawk -v THRESHOLD="$THRESHOLD" '($2=="-1") &&  ($1<THRESHOLD) {positiveMatch++} END {print positiveMatch}')
+TN=$(cat predictions/joint_hard_predictions_and_ground.txt | awk -v THRESHOLD="$THRESHOLD" '($2=="-1") &&  ($1<THRESHOLD) {positiveMatch++} END {print positiveMatch}')
 
-FP=$(cat predictions/joint_hard_predictions_and_ground.txt | mawk -v THRESHOLD="$THRESHOLD" '($2=="-1") &&  ($1>=THRESHOLD) {positiveMatch++} END {print positiveMatch}')
+FP=$(cat predictions/joint_hard_predictions_and_ground.txt | awk -v THRESHOLD="$THRESHOLD" '($2=="-1") &&  ($1>=THRESHOLD) {positiveMatch++} END {print positiveMatch}')
 
-FN=$(cat predictions/joint_hard_predictions_and_ground.txt | mawk -v THRESHOLD="$THRESHOLD" '($2=="1") &&  ($1<THRESHOLD) {positiveMatch++} END {print positiveMatch}')
+FN=$(cat predictions/joint_hard_predictions_and_ground.txt | awk -v THRESHOLD="$THRESHOLD" '($2=="1") &&  ($1<THRESHOLD) {positiveMatch++} END {print positiveMatch}')
+
+LOGLOSS_FRAME="./predictions/joint_hard_predictions_and_ground.txt";
 
 compute_main_metrics
-echo -e "FW-hard-test\t$THRESHOLD\t$PRECISION\t$RECALL\t$F1\t$BALANCED_ACCURACY"
+echo -e "FW-hard-test\t$THRESHOLD\t$PRECISION\t$RECALL\t$F1\t$BALANCED_ACCURACY\t$LOGLOSS"
 
 if [ 1 -eq "$(echo "($BALANCED_ACCURACY - $MARGIN_OF_PERFORMANCE_HARD_TEST_BA) > 0" | bc)" ]; then
 	echo "$INFO_STRING FW learned much better than random (on hard test), exiting gracefully."
