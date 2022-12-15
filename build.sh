bd915588e817c9891aff3b118f7c6955e75e8b7b
bskrlj <skblaz@github.com>
2022-12-15 07:26:12 +0100

    Build instruction added

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+################################################################################################
+# This file serves as an entrypoint for building the binary with specific rustc flags.		   #
+# If there are flags you would like to test out, simply add them to RUSTFLAGS env. By default, #
+# no flags are used (generic release build)													   #
+################################################################################################
+
 cargo build --release;
+
+# Using specific flags examples
 #RUSTFLAGS="-Ctarget-cpu=skylake" cargo build --release;
 #RUSTFLAGS="-Ctarget-cpu=cascadelake" cargo build --release;
