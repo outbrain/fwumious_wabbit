1fb3d2bdc258d06cd4c5b090b5d1d86e671d7552
bskrlj <bskrlj@outbrain.com>
2023-10-05 08:43:16 +0200

    optimizer

--- src/optimizer.rs
+++ src/optimizer.rs
@@ -153,13 +153,11 @@ impl OptimizerTrait for OptimizerAdamDS {
 	if gradient == 0.0 {return 0.0};
 	data.grad_store = self.beta1 * data.grad_store + (1.0 - self.beta1) * gradient;
 	data.var_store = self.beta2 * data.var_store + (1.0 - self.beta2) * gradient.powf(2.0);
-
 	let update = self.learning_rate * (data.grad_store * inv_sqrt32_plus_eps(data.var_store));
 
-        if update.is_nan() || update.is_infinite() {
-            return self.learning_rate * update.signum();
+        if update.is_nan() || update.is_infinite() || update.abs() < 1e-5 {
+	    return 0.0;
         }
-	
 	return update;
 
     }
