bd8911f9511974e1cb3c98e2003bfc7283fb8c0d
ggaspersic <grega.gaspersic@outbrain.com>
2023-10-11 11:08:29 +0200

    Small correction

--- src/block_monte_carlo.rs
+++ src/block_monte_carlo.rs
@@ -1,7 +1,7 @@
 use std::any::Any;
 use std::error::Error;
 
-use rand::distributions::{Distribution, Uniform};
+use rand::distributions::{Distribution, Open01, Uniform};
 use rand_xoshiro::rand_core::SeedableRng;
 use rand_xoshiro::Xoshiro256Plus;
 
@@ -26,11 +26,14 @@ pub fn new_monte_carlo_block(
 
     let number_of_inputs_to_skip = (dropout_rate * num_inputs as f32) as usize;
     let skip_index_generator = Uniform::from(0..num_inputs);
+    let increment_generator = Open01 {};
 
     let block = Box::new(BlockMonteCarlo {
         num_iterations,
         number_of_inputs_to_skip,
+        dropout_rate,
         skip_index_generator,
+        increment_generator,
         output_offset: usize::MAX,
         input_offset: usize::MAX,
         num_inputs,
@@ -47,7 +50,9 @@ fn create_seed_from_input_tape(input_tape: &[f32]) -> u64 {
 pub struct BlockMonteCarlo {
     num_iterations: usize,
     number_of_inputs_to_skip: usize,
+    dropout_rate: f32,
     skip_index_generator: Uniform<usize>,
+    increment_generator: Open01,
 
     pub num_inputs: usize,
     pub input_offset: usize,
@@ -115,7 +120,12 @@ impl BlockTrait for BlockMonteCarlo {
 
                 output_tape.copy_from_slice(&input_tape);
                 if i != 0 {
-                    for _ in 0..self.number_of_inputs_to_skip {
+                    let mut number_of_inputs_to_skip = self.number_of_inputs_to_skip;
+                    let sample = self.increment_generator.sample(&mut rng);
+                    if self.dropout_rate >= sample {
+                        number_of_inputs_to_skip += 1;
+                    }
+                    for _ in 0..number_of_inputs_to_skip {
                         let skip_index = self.skip_index_generator.sample(&mut rng);
                         *output_tape.get_unchecked_mut(skip_index) = 0.0;
                     }
@@ -160,7 +170,12 @@ impl BlockTrait for BlockMonteCarlo {
 
                 output_tape.copy_from_slice(&input_tape);
                 if i != 0 {
-                    for _ in 0..self.number_of_inputs_to_skip {
+                    let mut number_of_inputs_to_skip = self.number_of_inputs_to_skip;
+                    let sample = self.increment_generator.sample(&mut rng);
+                    if self.dropout_rate >= sample {
+                        number_of_inputs_to_skip += 1;
+                    }
+                    for _ in 0..number_of_inputs_to_skip {
                         let skip_index = self.skip_index_generator.sample(&mut rng);
                         *output_tape.get_unchecked_mut(skip_index) = 0.0;
                     }
@@ -204,6 +219,7 @@ impl BlockMonteCarlo {
     }
 
     fn fill_stats(&self, pb: &mut PortBuffer) {
+        println!("{:?}", pb.observations);
         let mean: f32 = pb.observations.iter().sum::<f32>() / self.num_iterations as f32;
         let variance = pb
             .observations
@@ -310,16 +326,53 @@ mod tests {
 
         spredict2(&mut bg, &fb, &mut pb);
         let expected_observations = [
-            2.0, 4.0, 4.0, 5.0, 2.0, 4.0, 0.0, 5.0, 0.0, 4.0, 4.0, 5.0, 2.0, 4.0, 4.0, 5.0
+            2.0, 4.0, 4.0, 5.0, 2.0, 0.0, 4.0, 5.0, 2.0, 0.0, 4.0, 5.0, 2.0, 4.0, 4.0, 5.0
         ];
-        assert_eq!(pb.observations.len(), expected_observations.len());
         assert_eq!(pb.observations, expected_observations);
 
         assert_ne!(pb.stats, None);
 
         let stats = pb.stats.unwrap();
-        assert_epsilon!(stats.mean, 13.0);
-        assert_epsilon!(stats.variance, 392.33334);
-        assert_epsilon!(stats.standard_deviation, 19.807405);
+        assert_epsilon!(stats.mean, 12.333333);
+        assert_epsilon!(stats.variance, 354.55554);
+        assert_epsilon!(stats.standard_deviation, 18.829645);
+    }
+
+    #[test]
+    fn test_monte_carlo_block_with_multiple_runs_and_high_dropout_rate() {
+        let mi = ModelInstance::new_empty().unwrap();
+        let mut bg = BlockGraph::new();
+        let input_block = block_misc::new_const_block(&mut bg, vec![2.0, 4.0, 4.0, 5.0]).unwrap();
+        let observe_block_backward =
+            block_misc::new_observe_block(&mut bg, input_block, Observe::Backward, None).unwrap();
+        let triangle_block =
+            new_monte_carlo_block(&mut bg, observe_block_backward, 3, 0.7).unwrap();
+        let observe_block_forward =
+            block_misc::new_observe_block(&mut bg, triangle_block, Observe::Forward, None).unwrap();
+        block_misc::new_sink_block(
+            &mut bg,
+            observe_block_forward,
+            block_misc::SinkType::Untouched,
+        )
+            .unwrap();
+        bg.finalize();
+        bg.allocate_and_init_weights(&mi);
+
+        let mut pb = bg.new_port_buffer();
+        let fb = fb_vec();
+        slearn2(&mut bg, &fb, &mut pb, true);
+        assert_eq!(pb.observations, [2.0, 4.0, 4.0, 5.0, 2.0, 4.0, 4.0, 5.0]);
+        spredict2(&mut bg, &fb, &mut pb);
+        let expected_observations = [
+            2.0, 4.0, 4.0, 5.0, 2.0, 0.0, 0.0, 5.0, 2.0, 0.0, 0.0, 0.0, 2.0, 4.0, 4.0, 5.0
+        ];
+        assert_eq!(pb.observations, expected_observations);
+
+        assert_ne!(pb.stats, None);
+
+        let stats = pb.stats.unwrap();
+        assert_epsilon!(stats.mean, 8.0);
+        assert_epsilon!(stats.variance, 159.33333);
+        assert_epsilon!(stats.standard_deviation, 12.62273);
     }
 }
