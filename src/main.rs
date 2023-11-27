c604e9f694b511eea76072f7cb939645b1fd6b13
bskrlj <bskrlj@outbrain.com>
2023-11-27 07:06:46 +0100

    quantization internal for convert

--- src/main.rs
+++ src/main.rs
@@ -148,6 +148,9 @@ fn main2() -> Result<(), Box<dyn Error>> {
         let (mut mi2, vw2, re_fixed) =
             new_regressor_from_filename(filename, true, Option::Some(&cl))?;
         mi2.optimizer = Optimizer::SGD;
+	if cl.is_present("weight_quantization") {
+	    mi2.dequantize_weights = true;
+	}
         if let Some(filename1) = inference_regressor_filename {
             save_regressor_to_filename(filename1, &mi2, &vw2, re_fixed, quantize_weights).unwrap()
         }
--- src/quantization.rs
+++ src/quantization.rs
@@ -2,15 +2,19 @@
 use std::slice;
 use std::io;
 
+const BY_3: usize = 3;
 
-pub fn quantize_ffm_weights_3by(weights: &[f32]) -> Vec<[u8; 3]> {
+pub fn quantize_ffm_weights_3by(weights: &[f32]) -> Vec<[u8; BY_3]> {
     // Quantize float-based weights to three most significant bytes
 
-    let mut v = Vec::<[u8; 3]>::with_capacity(weights.len());
+    let mut v = Vec::<[u8; BY_3]>::with_capacity(weights.len());
     for &weight in weights {
         let tmp_bytes = weight.to_be_bytes();
-        let tmp_vec = [tmp_bytes[0], tmp_bytes[1], tmp_bytes[2]];
-        v.push(tmp_vec);
+	let mut out_ary: [u8; BY_3] = [0; BY_3];
+	for k in 0..BY_3 {
+	    out_ary[k] = tmp_bytes[k];
+	}
+        v.push(out_ary);
     }
     debug_assert_eq!(v.len(), weights.len());
     v
@@ -23,23 +27,17 @@ pub fn dequantize_ffm_weights_3by(input_bufreader: &mut dyn io::Read, reference_
     unsafe {
         let buf_view: &mut [u8] = slice::from_raw_parts_mut(
 	    reference_weights.as_mut_ptr() as *mut u8,
-	    reference_weights.len() * 3,
+	    reference_weights.len() * BY_3,
         );
         let _ = input_bufreader.read_exact(buf_view);
 
-        let tmp_weights: Vec<u8> = buf_view
-            .chunks(3)
-            .flat_map(|chunk| chunk.iter().chain(std::iter::once(&0u8)).cloned())
-            .collect();
+	let mut out_ary: [u8; 4] = [0; 4];
+	for (chunk, float_ref) in buf_view.chunks(3).zip(reference_weights.iter_mut()) {
 
-        for (chunk, float_ref) in tmp_weights.chunks(4).zip(reference_weights.iter_mut()) {
-
- 	    let mut out_ary: [u8; 4] = [0; 4];
-	    out_ary[0] = chunk[0];
-	    out_ary[1] = chunk[1];
-	    out_ary[2] = chunk[2];
-	    
-            *float_ref = f32::from_be_bytes(out_ary);
-        }
-    }
+	    for k in 0..BY_3 {
+		out_ary[k] = chunk[k];
+	    }
+	    *float_ref = f32::from_be_bytes(out_ary);
+	}
+    }	
 }
